2cd674cab376c52294c42dbd273bb81699540d4e
kasireddymukesh <kasireddymukesh7@gmail.com>
2026-05-22 15:23:15 +0530

    logout function api in headevue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,43 @@
 <template>
-<div class="nav">
-<a href="#">Home</a>
-<a href="#">Add Resturant</a>
-<a href="#">Update Resturant</a>
-<a href="#">Logout</a>
-</div>
+  <div class="nav">
+    <a href="#">Home</a>
+    <a href="#">Add Restaurant</a>
+    <a href="#">Update Restaurant</a>
+    <a v-on:click="logout" href="#">Logout</a>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.$router.push({name:'LoginPage'})
+    }
+  }
 }
 </script>
-<style>
-.nav{
-    background-color: #333;
-    overflow:hidden
-}
-.nav a {
-    float: left;
-    color:#f2f2f2f2;
-    padding:14px 16px;
-    text-align:center;
-    font-size:17px;
-    text-decoration:none;
-    margin-right: 5px;
-}
-.nav a:hover {
-    background: #ddd;
-    color:#333;
 
+<style>
+.nav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+.nav a {
+  float: left;
+  color: #f2f2f2;
+  padding: 14px 16px;
+  text-align: center;
+  font-size: 17px;
+  text-decoration: none;
+  margin-right: 5px;
+}
+
+.nav a:hover {
+  background: #ddd;
+  color: #333;
 }
 </style>
