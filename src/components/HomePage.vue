f27d7c6de1d32acbebd10f894d58e846801af9e3
kasireddymukesh <kasireddymukesh7@gmail.com>
2026-05-22 15:00:42 +0530

    headerpagecreation

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,15 +1,24 @@
 <template>
+  <AppHeader />
   <h1>Hello user, Welcome on Home Page</h1>
 </template>
 
 <script>
+import AppHeader from './Header.vue'
+
 export default {
   name: 'HomePage',
-  mounted(){
-     let user = localStorage.getItem('user-info');
-     if(!user){
-        this.$router.push({ name: "SignUp" });
-     }
+
+  components: {
+    AppHeader
+  },
+
+  mounted() {
+    let user = localStorage.getItem('user-info');
+
+    if (!user) {
+      this.$router.push({ name: "SignUp" });
+    }
   }
 }
 </script>
